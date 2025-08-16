8fd0382f3539f3f43fcb7ac2dd83295a618dbf46
HEAD-FACE <ac.headface@gmail.com>
2025-08-16 21:57:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ response_type=code
     const statusPending = document.getElementById('status-pending');
     const statusCompleted = document.getElementById('status-completed');
     const statusText = document.getElementById('status-text');
-    const backToMainButton = document.getElementById('back-to-main');
+ 
     
     // ดึงข้อมูลผู้ใช้จาก Local Storage
     const userData = JSON.parse(localStorage.getItem('user'));
@@ -90,7 +90,7 @@ response_type=code
             stepSubmit.style.display = 'none';
             statusText.innerText = 'ลงทะเบียนสำเร็จแล้ว!';
             // เพิ่มปุ่มกลับหน้าหลัก
-            backToMainButton.style.display = 'block';
+
         } else {
             statusRegister.classList.add('active');
             backToMainButton.style.display = 'none';
@@ -189,9 +189,7 @@ response_type=code
         }
     });
 
-    backToMainButton.addEventListener('click', () => {
-        window.location.href = 'index.html';
-    });
+    
     
     initializePage();
 });
