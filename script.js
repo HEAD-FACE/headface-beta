7baee8cdfe89e863d0e4acd13e6499e88e51aa1c
HEAD-FACE <ac.headface@gmail.com>
2025-08-17 17:10:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -48,7 +48,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-
 document.addEventListener('DOMContentLoaded', () => {
     // กำหนด URL ของ GAS Web App และ LINE Login
     const gasUrl = 'https://script.google.com/macros/s/AKfycbxQyMf_zMNuZoa_JLqa2S5LJYgxd1HwDfnMw-3_FtMH-mN2Db72O4xfqpU17zg2mebPkw/exec';
@@ -73,7 +72,7 @@ response_type=code
     const statusPending = document.getElementById('status-pending');
     const statusCompleted = document.getElementById('status-completed');
     const statusText = document.getElementById('status-text');
- 
+    const backToMainButton = document.getElementById('back-to-main');
     
     // ดึงข้อมูลผู้ใช้จาก Local Storage
     const userData = JSON.parse(localStorage.getItem('user'));
@@ -109,7 +108,8 @@ response_type=code
             stepPayment.style.display = 'none';
             stepSubmit.style.display = 'none';
             statusText.innerText = 'อยู่ระหว่างรอการตรวจสอบ';
-
+            // เพิ่มปุ่มกลับหน้าหลัก
+            backToMainButton.style.display = 'block'; 
         } else if (status === 'success') {
             statusRegister.classList.add('active');
             statusPending.classList.remove('active');
@@ -121,10 +121,10 @@ response_type=code
             stepSubmit.style.display = 'none';
             statusText.innerText = 'ลงทะเบียนสำเร็จแล้ว!';
             // เพิ่มปุ่มกลับหน้าหลัก
-
+            backToMainButton.style.display = 'block';
         } else {
             statusRegister.classList.add('active');
-
+            backToMainButton.style.display = 'none';
         }
     }
 
@@ -220,7 +220,9 @@ response_type=code
         }
     });
 
-    
+    backToMainButton.addEventListener('click', () => {
+        window.location.href = 'index.html';
+    });
     
     initializePage();
 });
